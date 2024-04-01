03f239dc285ab46448fb2abe594a2947e94dcd04
azizulweb <muhanifazizulfikri@gmail.com>
2024-04-01 22:20:04 +0700

    folder baru belajar-php

---
+++ listingproduct.php
@@ -0,0 +1,102 @@
+<?php 
+$products =
+[
+    // 1
+    [ 
+       "menu" => "Siomay",
+       "img" => "images/breafaet_4.jpg",
+       "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+       "price" => "20.000"
+
+    ],
+    // 2
+    [
+        "menu" => "Vegetable Sunrise",
+        "img" => "images/breafaet_2.jpg",
+        "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        "price" => "20.000"
+ 
+    ],
+    // 3
+    [
+        "menu" => "Pie Berry",
+        "img" => "images/breakfeast_3.jpg",
+        "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        "price" => "20.000"
+ 
+    ],
+    // 4
+    [
+        "menu" => "Black Coffe",
+        "img" => "images/bg.kopihitam.jpg",
+        "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        "price" => "20.000"
+ 
+    ],
+    // 5
+    [
+        "menu" => "MilkShake Oreo",
+        "img" => "images/milkshake.jpg",
+        "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        "price" => "20.000"
+ 
+    ],
+    // 6
+    [
+        "menu" => "Coffe Latte",
+        "img" => "images/exxpresso.jpg",
+        "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        "price" => "20.000"
+ 
+     ],
+    // 7
+     [
+         "menu" => "Chiken Karee",
+         "img" => "images/maincourse4.jpg",
+         "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+         "price" => "20.000"
+  
+     ],
+    // 8
+     [
+         "menu" => "Steak Salad",
+         "img" => "images/maincourse1.jpg",
+         "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+         "price" => "20.000"
+  
+     ],
+    // 9
+     [
+         "menu" => "Satay",
+         "img" => "images/maincourse3.jpg",
+         "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+         "price" => "20.000"
+  
+     ],
+    // 10
+     [
+         "menu" => "Dessert Cake",
+         "img" => "images/dessert1.jpg",
+         "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+         "price" => "20.000"
+  
+     ],
+    // 11
+     [
+        "menu" => "Red Velvet",
+        "img" => "images/dessert3.jpg",
+        "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        "price" => "20.000"
+ 
+    ],
+    // 12
+    [
+        "menu" => "Matcha Dessert",
+        "img" => "images/dessert2.jpg",
+        "desc" => "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        "price" => "20.000"
+ 
+    ],
+ 
+]
+?>
